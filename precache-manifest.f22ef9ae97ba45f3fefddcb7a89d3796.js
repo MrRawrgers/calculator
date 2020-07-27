d1545d6ff02b8e409cd4f7c4ec0c1e46a92d4bed
MrRawrgers <niall.w.rogers@gmail.com>
2020-07-27 12:58:37 +0100

    Updates

---
+++ precache-manifest.f22ef9ae97ba45f3fefddcb7a89d3796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78db2f2b7c0851911da7164f2627aa0",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "87b6cd6bb66fc7a37891",
+    "url": "/calculator/static/css/main.c57d402c.chunk.css"
+  },
+  {
+    "revision": "114bbaea14242877659e",
+    "url": "/calculator/static/js/2.6912228e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.6912228e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87b6cd6bb66fc7a37891",
+    "url": "/calculator/static/js/main.b9f187d9.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
